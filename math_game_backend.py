47c5a6d54852a5db30d86eabf9e31cb160a077d8
artificialname <yotamweiner@gmail.com>
2025-02-05 14:24:25 -0500

    Update math_game_backend.py

--- math_game_backend.py
+++ math_game_backend.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -27,4 +28,6 @@ def validate_equation():
         return jsonify({"valid": False, "message": "Incorrect. Try again!"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use Render's dynamic port
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
